b1c0f0fc38e7a1f0c72ed6672b9a5a5e837f19f1
Suralya <Tina@Plavius.de>
2020-03-18 16:17:26 +0100

    Adding HeroScript Variables, GamemanagerObject with Tag, Material GrayBlack, Prefab StartArea
    Adjusting new Temp Hero fitting for Artstyle

---
+++ MobileGame/MobileProject/Assets/Scripts/Hero.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    public string Name = "HERO";
+    public int Lvl = 1;
+
+    private int maxExp = 100;
+    public int CurrentExp = 0;
+
+    public int Hp, Str, Def, Dex, Spe = 1;
+    public int StageCounter, MonsterCounter = 0;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

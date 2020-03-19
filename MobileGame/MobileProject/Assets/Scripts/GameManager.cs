5085efe68e83a4abe9c5d64eeed6e260795e2296
Suralya <Tina@Plavius.de>
2020-03-19 15:52:41 +0100

    Adding Background Texture, Additional Background-lighting, Edit GameManager Starting Sequence -> Setup fist tiles

--- MobileGame/MobileProject/Assets/Scripts/GameManager.cs
+++ MobileGame/MobileProject/Assets/Scripts/GameManager.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject StartingArea;
     public List<GameObject> Areas = new List<GameObject>();
+    private Queue<GameObject> areasInUse=new Queue<GameObject>();
+    private GameObject[] areasInUseArray = new GameObject[3];
+
+    private Vector3 position1 = new Vector3(0, 0, -1);
+    private Vector3 position2 = new Vector3(0, 0, 0);
+    private Vector3 position3 = new Vector3(0, 0, 1);
+
+    public bool newStepEnabled = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        areasInUse.Enqueue(StartingArea);
+        areasInUse.Enqueue(Areas[Random.Range(0, Areas.Count - 1)]);
+        areasInUseArray = areasInUse.ToArray();
+        Instantiate(areasInUseArray[0],position2,Quaternion.identity);
+        Instantiate(areasInUseArray[1], position3, Quaternion.identity);
+
     }
 
     // Update is called once per frame

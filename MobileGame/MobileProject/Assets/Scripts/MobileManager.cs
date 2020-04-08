a612d1e3d61ed4b864cb773242d76ac775628451
Suralya <Tina@Plavius.de>
2020-04-08 15:58:35 +0200

    Adding Touch

---
+++ MobileGame/MobileProject/Assets/Scripts/MobileManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileManager : MonoBehaviour
+{
+
+    //Script to set Camara accordng to Phone
+
+    public enum Device
+    {
+        SGS4,//1920x1080
+        SGS5,//1920x1080
+        SN8,//1280x800
+        SGS3//1280x720
+    }
+    public Device _device;
+
+
+    void Start()
+    {
+
+        //Camera.aspect = 16f/10f;
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+
+        switch (_device)
+        {
+
+            case (Device.SGS4):
+                {
+                    Screen.SetResolution(1920, 1080, true);
+                    break;
+                }
+            case (Device.SGS5):
+                {
+                    Screen.SetResolution(1920, 1080, true);
+                    break;
+                }
+            case (Device.SN8):
+                {
+                    Screen.SetResolution(1280, 800, true);
+                    break;
+                }
+            case (Device.SGS3):
+                {
+                    Screen.SetResolution(1280, 720, true);
+                    break;
+                }
+
+        }
+
+
+
+    }
+
+}
---
+++ MobileGame/MobileProject/Assets/Scripts/MouseDownTouch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+//	Allows "OnMouseDown()" events to work on touch devices.
+//From: http://wiki.unity3d.com/index.php/OnMouseDown?_ga=2.40046434.1765837225.1586277858-1854003708.1571771899
+/// </summary>
+public class MouseDownTouch : MonoBehaviour
+{
+    private void Update()
+    {
+        var hit = new RaycastHit();
+
+        for (int i = 0; i < Input.touchCount; ++i)
+        {
+            if (Input.GetTouch(i).phase.Equals(TouchPhase.Began))
+            {
+                // Construct a ray from the current touch coordinates.
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    hit.transform.gameObject.SendMessage("OnMouseDown");
+                }
+            }
+        }
+    }
+}

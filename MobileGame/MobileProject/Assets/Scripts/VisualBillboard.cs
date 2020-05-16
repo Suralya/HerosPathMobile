e9dadf8f62fa572f50b1d0f8b808938a3436f487
Suralya <Tina@Plavius.de>
2020-05-16 20:14:00 +0200

    adding Hero-Spritesheet

---
+++ MobileGame/MobileProject/Assets/Scripts/VisualBillboard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualBillboard : MonoBehaviour
+{
+    private Quaternion SetRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.rotation != SetRotation)
+        {
+            transform.rotation = SetRotation;
+        }
+    }
+}
